790332aa6c9c05e0e169245061588460bce9b56f
Enas Khaled <enas.ah.khaled@gmail.com>
2024-08-11 05:13:22 +0000

    delete

--- 5-photo-reject.js
+++
@@ -1,6 +0,0 @@
-export default function uploadPhoto(filename) {
-  return new Promise((resolve, reject) => {
-    // Reject the promise with an error and the formatted string
-    reject(new Error(`${filename} cannot be processed`));
-  });
-}
